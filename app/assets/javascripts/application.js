75701860b78a05e64baf5c9857ff017d9b3731b2
Pedro De Ona <pdeon003@fiu.edu>
2017-09-29 11:57:53 -0400

    ajaxed show

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//
 //= require rails-ujs
 //= require turbolinks
 //= require jquery/dist/jquery.min
+//= require bootstrap/dist/js/bootstrap.min
 //= require_tree .
